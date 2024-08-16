93ee4104bdb1eb0f99907b3d5e54be17513f5817
lazaro <lazaroalves12355@gmail.com>
2024-08-16 20:19:23 -0300

    v5

--- src/app/(private)/dashboard/gerenciamento_estoque/page.tsx
+++ src/app/(private)/dashboard/gerenciamento_estoque/page.tsx
@@ -11,9 +11,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-
 export default function GerenciamentoEstoque() {
-    const router = useRouter()
+    const router = useRouter();
     const { user, token } = useAuth();
     const { data: folders, loading, error } = useFetch<FolderProps[]>(`${BaseURL}folder/getAll/${user?.id}`);
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -33,10 +32,10 @@ export default function GerenciamentoEstoque() {
             });
 
             if (response.ok) {
-                router.refresh()
-                console.log('crio')
+                router.refresh();
+                console.log('Pasta criada com sucesso');
             } else {
-                console.log('erro')
+                console.log('Erro ao criar pasta');
             }
         } catch (error) {
             console.error('Erro ao criar pasta:', error);
@@ -56,12 +55,14 @@ export default function GerenciamentoEstoque() {
             if (!response.ok) {
                 throw new Error(`Erro na requisição: ${response.statusText}`);
             }
+            // Recarregar a página após a exclusão
+            router.refresh();
         } catch (error) {
-            console.error('Erro ao apagar o item do estoque:', error);
+            console.error('Erro ao apagar a pasta:', error);
         }
     };
 
-    if (loading) return <LoadingSpinner />
+    if (loading) return <LoadingSpinner />;
 
     return (
         <section>
@@ -70,84 +71,45 @@ export default function GerenciamentoEstoque() {
                 <button
                     onClick={() => setIsModalOpen(true)}
                     className="flex w-20 cursor-pointer lg:border border-border text-nowrap text-transparent lg:px-12 lg:py-1 lg:w-32 justify-center text-sm lg:text-base md:text-base rounded-md lg:bg-slate-200 lg:text-gray-600 lg:mr-5 mr-8">
-                    Criar Tarefa
+                    Criar Pasta
                     <Image
                         src="/icon/plus.svg"
-                        alt="Icone criar pasta"
+                        alt="Ícone criar pasta"
                         width={40}
                         height={40}
                         className="lg:hidden flex"
                     />
                 </button>
             </div>
-            <div className="w-80 lg:w-[1100px] lg:ml-6 md:w-[700px] md:m-auto md:pt-12  md:mb-4  pl-8 mt-4 h-[500px] mb-4 overflow-y-scroll">
+            <div className="w-80 lg:w-[1100px] lg:ml-6 md:w-[700px] md:m-auto md:pt-12 md:mb-4 pl-8 mt-4 h-[500px] mb-4 overflow-y-scroll">
                 <div className="flex gap-4 flex-wrap">
-                    {folders?.map((folder) => (
-                        <div className="relative h-fit cursor-pointer" key={folder.id}>
-                            <Link href={`/dashboard/gerenciamento_estoque/${folder.id}`}>
-                                <div className="flex lg:w-40 lg:h-40 w-32 h-32 text-center items-center justify-center border-border border rounded-2xl shadow-md cursor-pointer hover:scale-110 transition .3s ease-in">
-                                    <p>{folder.nome}</p>
-                                    <span className="absolute w-8 h-8 rounded-full border-2 bottom-1 right-2 bg-white flex items-center justify-center text-sm text-gray-700">
-                                        {folder._count.Tarefa}
-                                    </span>
+                    {folders && folders.length > 0 ? (
+                        folders.map((folder) => (
+                            <div className="relative h-fit cursor-pointer" key={folder.id}>
+                                <Link href={`/dashboard/gerenciamento_estoque/${folder.id}`}>
+                                    <div className="flex lg:w-40 lg:h-40 w-32 h-32 text-center items-center justify-center border-border border rounded-2xl shadow-md cursor-pointer hover:scale-110 transition .3s ease-in">
+                                        <p>{folder.nome}</p>
+                                        <span className="absolute w-8 h-8 rounded-full border-2 bottom-1 right-2 bg-white flex items-center justify-center text-sm text-gray-700">
+                                            {folder._count.Tarefa}
+                                        </span>
+                                    </div>
+                                </Link>
+                                <div className="absolute bottom-4 left-4 cursor-pointer" onClick={() => handleDelete(folder.id)}>
+                                    <Image
+                                        src="/icon/trash.svg"
+                                        alt="Ícone Deletar pasta"
+                                        width={20}
+                                        height={20}
+                                    />
                                 </div>
-                            </Link>
-                            <div className="absolute bottom-4 left-4 cursor-pointer" onClick={() => handleDelete(folder.id)}>
-                                <Image
-                                    src="/icon/trash.svg"
-                                    alt="Icone Deletar tarefa"
-                                    width={20}
-                                    height={20}
-                                />
                             </div>
-                        </div>
-                    ))}
+                        ))
+                    ) : (
+                        <p className="text-center w-full">Você não tem pastas criadas</p>
+                    )}
                 </div>
             </div>
             <Modal show={isModalOpen} onClose={() => setIsModalOpen(false)} onSubmit={handleCreateFolder} />
         </section>
     );
 }
-
-/**
- * 
- *  <section className="h-[650px] flex flex-col">
-            <div className="flex justify-between items-center border-b pl-zz lg:pl-0">
-                <h1 className="lg:text-lg m-4 md:text-2xl text-base w-44">Gerenciamento Estoque</h1>
-                <button
-                    onClick={() => setIsModalOpen(true)}
-                    className="flex cursor-pointer lg:border border-border w-0 text-transparent lg:w-32 justify-center text-sm lg:text-base md:text-base text-nowrap rounded-md lg:bg-slate-200 text-black lg:mr-20 mr-8">
-                    Criar Pasta
-                </button>
-            </div>
-            {folders && folders.length > 0 ? (
-                <div className="flex m-4 mt-8 lg:w-[1100px]  md:w-[750px]  w-80 items-center lg:items-start lg:justify-normal justify-center  overflow-y-auto h-max flex-row  flex-wrap p-3 gap-x-4 gap-y-4">
-                    {folders.map((folder) => (
-                        <div className="relative h-fit cursor-pointer" key={folder.id}>
-                            <Link href={`/dashboard/gerenciamento_estoque/${folder.id}`}>
-                                <div className="flex lg:w-40 lg:h-40 w-32 h-32 text-center items-center justify-center border-border border rounded-2xl shadow-md cursor-pointer hover:scale-110 transition .3s ease-in">
-                                    <p>{folder.nome}</p>
-                                    <span className="absolute w-8 h-8 rounded-full border-2 bottom-1 right-2 bg-white flex items-center justify-center text-sm text-gray-700">
-                                        {folder._count.Tarefa}
-                                    </span>
-                                </div>
-                            </Link>
-                            <div className="absolute bottom-4 left-4 cursor-pointer" onClick={() => handleDelete(folder.id)}>
-                                <Image
-                                    src="/icon/trash.svg"
-                                    alt="Icone Deletar tarefa"
-                                    width={20}
-                                    height={20}
-                                />
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            ) : (
-                <p className="m-4 text-gray-500">Nenhuma pasta encontrada.</p>
-            )}
-            <Modal show={isModalOpen} onClose={() => setIsModalOpen(false)} onSubmit={handleCreateFolder} />
-        </section>
- * 
- * 
- */
--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -15,17 +15,12 @@ export default function Dashboard() {
     const { dayName, day, fullYear, monthName } = useCurrentDayAndMonth();
     const { data: folders, loading, error } = useFetch<FolderProps[]>(`${BaseURL}folder/getAll/${user?.id}`);
     const { data: tasks } = useFetch<TaskProps[]>(`${BaseURL}task/user/${user?.id}`);
-    const [isClient, setIsClient] = useState(false)
-
-    useEffect(() => {
-        setIsClient(true)
-    }, [])
 
     return (
         <section className="lg:h-[650px] h-max pb-4 z-auto m-auto">
             <div className="border-b border-border py-4 lg:text-start text-end pr-5 lg:pr-0">
-                <h1 className="font-bold ml-7 mb-1">Olá, {isClient ? user?.nome : ""}</h1>
-                <p className="text-gray-500 ml-7 "> {dayName} {day} {monthName} de {fullYear}</p>
+                <h1 className="font-bold ml-7 mb-1">Olá, {user?.nome}</h1>
+                <p className="text-gray-500 ml-7"> {dayName} {day} {monthName} de {fullYear}</p>
             </div>
             {loading ? (
                 <LoadingDashboard />
@@ -39,33 +34,41 @@ export default function Dashboard() {
                             </Link>
                         </div>
                         <div className="lg:mt-0 mt-6 m-auto flex gap-2 lg:w-full lg:h-44 h-[270px] w-72 md:w-full overflow-hidden flex-wrap items-center">
-                            {folders?.slice(0, 8).map((item) => (
-                                <Link key={item.id} className="flex hover:scale-105 transition ease-in" href={`/dashboard/gerenciamento_estoque/${item.id}`}>
-                                    <div className="w-[130px] h-[130px] rounded-md text-center flex justify-center items-center m-auto bg-gray-200">
-                                        <p>{item.nome}</p>
-                                    </div>
-                                </Link>
-                            ))}
+                            {folders && folders.length > 0 ? (
+                                folders.slice(0, 8).map((item) => (
+                                    <Link key={item.id} className="flex hover:scale-105 transition ease-in" href={`/dashboard/gerenciamento_estoque/${item.id}`}>
+                                        <div className="w-[130px] h-[130px] rounded-md text-center flex justify-center items-center m-auto bg-gray-200">
+                                            <p>{item.nome}</p>
+                                        </div>
+                                    </Link>
+                                ))
+                            ) : (
+                                <p>Você não tem pastas criadas</p>
+                            )}
                         </div>
                     </div>
-                    <div className="mt-5 ">
+                    <div className="mt-5">
                         <div className="flex justify-between">
                             <h1 className="text-xl font-bold">Itens do Estoque</h1>
                             <Link href="/dashboard/itens_estoque" className="bg-gray-200 text-black px-4 rounded-md items-center justify-center flex mr-4 lg:mr-0">
                                 Ver Mais
                             </Link>
                         </div>
-                        <div className="mt-4 w-full ">
+                        <div className="mt-4 w-full">
                             <ul className="lg:h-[239px] h-[170px] gap-2 flex flex-col overflow-hidden mr-4">
-                                {tasks?.map((task) => (
-                                    <LiItensDoEstoque
-                                        id={task.id}
-                                        key={task.id}
-                                        titulo={task.nome}
-                                        perecivel={task.perecivel}
-                                        nomePasta={task.pasta.nome}
-                                    />
-                                ))}
+                                {tasks && tasks.length > 0 ? (
+                                    tasks.map((task) => (
+                                        <LiItensDoEstoque
+                                            id={task.id}
+                                            key={task.id}
+                                            titulo={task.nome}
+                                            perecivel={task.perecivel}
+                                            nomePasta={task.pasta.nome}
+                                        />
+                                    ))
+                                ) : (
+                                    <p>Você não tem itens no estoque</p>
+                                )}
                             </ul>
                         </div>
                     </div>
